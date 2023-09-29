94a5306fc17f6e1ef1e975fd35ada20ee2f7d37c
sid-alluri <sidalluri2605@gmail.com>
2023-09-28 22:12:25 -0500

    add: get numbers

--- gnarkcircuits/ecdsa_test.go
+++ gnarkcircuits/ecdsa_test.go
@@ -2,12 +2,18 @@ package gnarkcircuits
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
+	"reflect"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark-crypto/ecc/secp256k1/ecdsa"
-	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
 	"github.com/consensys/gnark/std/math/emulated"
 	gnarkecdsa "github.com/consensys/gnark/std/signature/ecdsa"
 	"github.com/consensys/gnark/test"
@@ -15,44 +21,69 @@ import (
 
 func TestECDSA(t *testing.T) {
 	assert := test.NewAssert(t)
-	secret_key, err := ecdsa.GenerateKey(rand.Reader)
-	assert.NoError(err)
+	exp_len := []int{10, 100, 1000}
+	for l := 0; l < len(exp_len); l++ {
+		secret_key, e := ecdsa.GenerateKey(rand.Reader)
+		assert.NoError(e)
 
-	m := []byte("UIUC Computer Science")
+		m := make([]byte, exp_len[l])
+		_, er := rand.Read(m)
+		assert.NoError(er)
 
-	signatureBytes, err := secret_key.Sign(m, nil)
-	assert.NoError(err)
+		signatureBytes, e := secret_key.Sign(m, nil)
+		assert.NoError(e)
 
-	var signature ecdsa.Signature // Reference: https://github.com/Consensys/gnark/blob/master/std/signature/ecdsa/ecdsa_test.go
-	signature.SetBytes(signatureBytes)
-	var r_bigInt, s_bigInt big.Int
-	r_bigInt.SetBytes(signature.R[:32])
-	s_bigInt.SetBytes(signature.S[:32])
+		var signature ecdsa.Signature // Reference: https://github.com/Consensys/gnark/blob/master/std/signature/ecdsa/ecdsa_test.go
+		signature.SetBytes(signatureBytes)
+		var r_bigInt, s_bigInt big.Int
+		r_bigInt.SetBytes(signature.R[:32])
+		s_bigInt.SetBytes(signature.S[:32])
 
-	m_bigInt := ecdsa.HashToInt(m)
+		m_bigInt := ecdsa.HashToInt(m)
 
-	// Objects for Instantiation
-	var msg_object emulated.Element[emulated.Secp256k1Fr]
-	var sig_object gnarkecdsa.Signature[emulated.Secp256k1Fr]
-	var pk_object gnarkecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]
+		// Objects for Instantiation
+		var msg_object emulated.Element[emulated.Secp256k1Fr]
+		var sig_object gnarkecdsa.Signature[emulated.Secp256k1Fr]
+		var pk_object gnarkecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]
 
-	circuit := &ECDSACircuit{
-		Message:   msg_object,
-		Signature: sig_object,
-		PublicKey: pk_object,
+		ckt := ECDSACircuit{
+			Message:   msg_object,
+			Signature: sig_object,
+			PublicKey: pk_object,
+		}
+
+		witness := ECDSACircuit{
+			Message: emulated.ValueOf[emulated.Secp256k1Fr](m_bigInt),
+			Signature: gnarkecdsa.Signature[emulated.Secp256k1Fr]{
+				R: emulated.ValueOf[emulated.Secp256k1Fr](r_bigInt),
+				S: emulated.ValueOf[emulated.Secp256k1Fr](s_bigInt),
+			},
+			PublicKey: gnarkecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
+				X: emulated.ValueOf[emulated.Secp256k1Fp](secret_key.PublicKey.A.X),
+				Y: emulated.ValueOf[emulated.Secp256k1Fp](secret_key.PublicKey.A.Y),
+			},
+		}
+		wtns, _ := frontend.NewWitness(&witness, ecc.BN254.ScalarField())
+		cs, _ := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &ckt)
+		srs, _ := test.NewKZGSRS(cs)
+		pK, vK, _ := plonk.Setup(cs, srs)
+		prover_start := time.Now()
+		proof, _ := plonk.Prove(cs, pK, wtns)
+		prover_time := time.Since(prover_start)
+		proof_size := reflect.TypeOf(proof).Size()
+		pubwtns, _ := wtns.Public()
+		verifier_start := time.Now()
+		err := plonk.Verify(proof, vK, pubwtns)
+		verifier_time := time.Since(verifier_start)
+		assert.Equal(err, nil)
+		fmt.Println("Number of constraints: ", cs.GetNbConstraints())
+		fmt.Println("Prover time: ", prover_time)
+		fmt.Println("Verifier time: ", verifier_time)
+		fmt.Println("Proof size: ", proof_size, "B")
+		var mem runtime.MemStats
+		runtime.ReadMemStats(&mem)
+		fmt.Println("Memory usage: ", mem.Sys/1024, "KB")
 	}
 
-	witness := &ECDSACircuit{
-		Message: emulated.ValueOf[emulated.Secp256k1Fr](m_bigInt),
-		Signature: gnarkecdsa.Signature[emulated.Secp256k1Fr]{
-			R: emulated.ValueOf[emulated.Secp256k1Fr](r_bigInt),
-			S: emulated.ValueOf[emulated.Secp256k1Fr](s_bigInt),
-		},
-		PublicKey: gnarkecdsa.PublicKey[emulated.Secp256k1Fp, emulated.Secp256k1Fr]{
-			X: emulated.ValueOf[emulated.Secp256k1Fp](secret_key.PublicKey.A.X),
-			Y: emulated.ValueOf[emulated.Secp256k1Fp](secret_key.PublicKey.A.Y),
-		},
-	}
-
-	assert.ProverSucceeded(circuit, witness, test.WithBackends(backend.PLONK), test.WithCurves(ecc.BN254))
+	// assert.ProverSucceeded(circuit, witness, test.WithBackends(backend.PLONK), test.WithCurves(ecc.BN254))
 }
--- gnarkcircuits/merkle_test.go
+++ gnarkcircuits/merkle_test.go
@@ -2,10 +2,16 @@ package gnarkcircuits
 
 import (
 	"crypto/rand"
+	"fmt"
+	"reflect"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/consensys/gnark-crypto/ecc"
-	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/backend/plonk"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/frontend/cs/scs"
 	"github.com/consensys/gnark/std/math/uints"
 	"github.com/consensys/gnark/test"
 	"golang.org/x/crypto/sha3"
@@ -13,40 +19,60 @@ import (
 
 func TestMerkle(t *testing.T) {
 	assert := test.NewAssert(t)
+	exp_len := []int{10, 100, 1000}
+	for l := 0; l < len(exp_len); l++ {
+		leaf := make([]byte, 1) // Reference: https://github.com/Consensys/gnark/tree/master/std/hash/sha3
+		_, e := rand.Reader.Read(leaf)
+		assert.NoError(e)
 
-	leaf := make([]byte, 1) // Reference: https://github.com/Consensys/gnark/tree/master/std/hash/sha3
-	_, err := rand.Reader.Read(leaf)
-	assert.NoError(err)
+		path := make([][]uints.U8, exp_len[l])
+		path_object := make([][]uints.U8, exp_len[l])
 
-	buf := make([]byte, 1)
-	_, err = rand.Reader.Read(buf)
-	assert.NoError(err)
+		hashFunc := sha3.New256()
+		hashFunc.Write(leaf)
 
-	path := make([][]uints.U8, 5)
-	path_object := make([][]uints.U8, 5)
+		for i := 0; i < exp_len[l]; i++ {
+			buf := make([]byte, 1)
+			_, err := rand.Reader.Read(buf)
+			assert.NoError(err)
+			path[i] = uints.NewU8Array(buf)
+			path_object[i] = make([]uints.U8, 1)
+			hashFunc.Write(buf)
+		}
+		digest := hashFunc.Sum(nil)
 
-	hashFunc := sha3.New256()
-	hashFunc.Write(leaf)
-
-	for i := 0; i < 5; i++ {
-		buf := make([]byte, 1)
-		_, err := rand.Reader.Read(buf)
-		assert.NoError(err)
-		path[i] = uints.NewU8Array(buf)
-		path_object[i] = make([]uints.U8, 1)
-		hashFunc.Write(buf)
+		ckt := MerkleTreeCircuit{
+			Leaf: make([]uints.U8, len(leaf)),
+			Path: path_object,
+			Root: make([]uints.U8, len(digest)),
+		}
+		witness := MerkleTreeCircuit{
+			Leaf: uints.NewU8Array(leaf),
+			Path: path,
+			Root: uints.NewU8Array(digest),
+		}
+		wtns, _ := frontend.NewWitness(&witness, ecc.BN254.ScalarField())
+		cs, _ := frontend.Compile(ecc.BN254.ScalarField(), scs.NewBuilder, &ckt)
+		srs, _ := test.NewKZGSRS(cs)
+		pK, vK, _ := plonk.Setup(cs, srs)
+		prover_start := time.Now()
+		proof, _ := plonk.Prove(cs, pK, wtns)
+		prover_time := time.Since(prover_start)
+		proof_size := reflect.TypeOf(proof).Size()
+		pubwtns, _ := wtns.Public()
+		verifier_start := time.Now()
+		err := plonk.Verify(proof, vK, pubwtns)
+		verifier_time := time.Since(verifier_start)
+		assert.Equal(err, nil)
+		fmt.Println("Number of path elements: ", exp_len[l])
+		fmt.Println("Number of constraints: ", cs.GetNbConstraints())
+		fmt.Println("Prover time: ", prover_time)
+		fmt.Println("Verifier time: ", verifier_time)
+		fmt.Println("Proof size: ", proof_size, "B")
+		var mem runtime.MemStats
+		runtime.ReadMemStats(&mem)
+		fmt.Println("Memory usage: ", mem.Sys/1024, "KB")
 	}
-	digest := hashFunc.Sum(nil)
 
-	circuit := &MerkleTreeCircuit{
-		Leaf: make([]uints.U8, len(leaf)),
-		Path: path_object,
-		Root: make([]uints.U8, len(digest)),
-	}
-	witness := &MerkleTreeCircuit{
-		Leaf: uints.NewU8Array(leaf),
-		Path: path,
-		Root: uints.NewU8Array(digest),
-	}
-	assert.ProverSucceeded(circuit, witness, test.WithBackends(backend.PLONK), test.WithCurves(ecc.BN254))
+	// assert.ProverSucceeded(circuit, witness, test.WithBackends(backend.PLONK), test.WithCurves(ecc.BN254))
 }
