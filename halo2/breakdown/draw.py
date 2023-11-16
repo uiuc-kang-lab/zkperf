0582f748b98bced92ea8e9ff67487a876024418c
span14 <span14@illinois.edu>
2023-11-15 21:31:55 -0600

    add experiments

--- halo2/breakdown/draw.py
+++ halo2/breakdown/draw.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import math
 import matplotlib.pyplot as plt
 import numpy as np
 import os
---
+++ halo2/zkml/col_experiments.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+OUTPUT_DIR="experiments"
+
+if [ ! -d "$OUTPUT_DIR" ]; then
+    mkdir -p "$OUTPUT_DIR"
+fi
+
+if [ ! -d "./params_kzg" ]; then
+    echo "No params directory found. Creating the directory..."
+    mkdir -p "./params_kzg"
+fi
+
+cargo build --release
+
+NUM_COLS=120
+K=15
+
+for i in $(seq $NUM_COLS -6 6)
+do
+    echo "Test DLRM with $i Columns"
+    python3 python/converter.py --model examples/dlrm/dlrm_mlp_checked_float32.tflite --model_output examples/dlrm/dlrm_model.msgpack --config python/dlrm_config.msgpack --scale_factor 512 --num_cols $i --num_witness_cols $(expr $i / 2) --k $K
+    ./target/release/time_circuit examples/dlrm/dlrm_model.msgpack examples/dlrm/dlrm_input.msgpack kzg > $OUTPUT_DIR/dlrm_test_$i_$K.txt 2>error.txt
+    if [ $(cat error.txt | grep -c "NotEnoughRowsAvailable") -ne 0 ]; then
+        rm $OUTPUT_DIR/dlrm_test_$i_$K.txt
+        rm error.txt
+        K=$(expr $K + 1)
+        python3 python/converter.py --model examples/dlrm/dlrm_mlp_checked_float32.tflite --model_output examples/dlrm/dlrm_model.msgpack --config python/dlrm_config.msgpack --scale_factor 512 --num_cols $i --num_witness_cols $(expr $i / 2) --k $K
+        ./target/release/time_circuit examples/dlrm/dlrm_model.msgpack examples/dlrm/dlrm_input.msgpack kzg > $OUTPUT_DIR/dlrm_test_$i_$K.txt 2>error.txt
+    fi
+done
