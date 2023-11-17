4b8f4a9b71ae86307a8a9d817d352b0a17d6aa2f
span14 <span14@illinois.edu>
2023-11-16 19:58:54 -0600

    update script

--- halo2/zkml/col_experiments.sh
+++ halo2/zkml/col_experiments.sh
@@ -16,7 +16,7 @@ cargo build --release
 NUM_COLS=120
 K=15
 
-for i in $(seq $NUM_COLS -6 100)
+for i in $(seq $NUM_COLS -6 6)
 do
     echo "Test DLRM with $i Columns"
     python3 python/converter.py --model examples/dlrm/dlrm_mlp_checked_float32.tflite --model_output examples/dlrm/dlrm_model.msgpack --config python/dlrm_config.msgpack --scale_factor 512 --num_cols "$i" --num_witness_cols $(expr "$i" / 2) --k "$K"
