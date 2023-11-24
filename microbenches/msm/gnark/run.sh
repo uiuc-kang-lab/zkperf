402ffd8060ad367f6189a6a4ea68671e3bb1a0d4
Alluri Siddhartha <lalluri2@illinois.edu>
2023-11-24 16:26:35 -0600

    change script

--- microbenches/msm/gnark/run.sh
+++ microbenches/msm/gnark/run.sh
@@ -1,2 +1,13 @@
 go build -o gmt
-./gmt msm 100
+
+./gmt msm 1000
+./gmt msm 10000
+./gmt msm 100000
+./gmt msm 1000000
+./gmt msm 10000000
+
+./gmt circuit 1000
+./gmt circuit 10000
+./gmt circuit 100000
+./gmt circuit 1000000
+./gmt circuit 10000000
