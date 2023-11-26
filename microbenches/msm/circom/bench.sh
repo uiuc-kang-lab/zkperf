8a3373e8206f308b64e40f1d75d2e465be64299a
Alluri Siddhartha <lalluri2@illinois.edu>
2023-11-26 16:28:07 -0600

    modify circom script

--- microbenches/msm/circom/bench.sh
+++ microbenches/msm/circom/bench.sh
@@ -1,4 +1,53 @@
 #!/bin/bash
 
 
-node ../../../circom/snarkjs/msm.js 100000
+node ../../../circom/snarkjs/msm.js 1024
+node ../../../circom/snarkjs/msm.js 2048
+node ../../../circom/snarkjs/msm.js 4096
+node ../../../circom/snarkjs/msm.js 8192
+node ../../../circom/snarkjs/msm.js 16384
+node ../../../circom/snarkjs/msm.js 32768
+node ../../../circom/snarkjs/msm.js 65536
+node ../../../circom/snarkjs/msm.js 131072
+node ../../../circom/snarkjs/msm.js 262144
+node ../../../circom/snarkjs/msm.js 524288
+node ../../../circom/snarkjs/msm.js 1048576
+node ../../../circom/snarkjs/msm.js 2097152
+node ../../../circom/snarkjs/msm.js 4194304
+node ../../../circom/snarkjs/msm.js 8388608
+
+node ../../../circom/snarkjs/fft.js 1024
+node ../../../circom/snarkjs/fft.js 2048
+node ../../../circom/snarkjs/fft.js 4096
+node ../../../circom/snarkjs/fft.js 8192
+node ../../../circom/snarkjs/fft.js 16384
+node ../../../circom/snarkjs/fft.js 32768
+node ../../../circom/snarkjs/fft.js 65536
+node ../../../circom/snarkjs/fft.js 131072
+node ../../../circom/snarkjs/fft.js 262144
+node ../../../circom/snarkjs/fft.js 524288
+node ../../../circom/snarkjs/fft.js 1048576
+node ../../../circom/snarkjs/fft.js 2097152
+node ../../../circom/snarkjs/fft.js 4194304
+node ../../../circom/snarkjs/fft.js 8388608
+
+N=1024
+R=13
+
+for i in $(seq 0 1 $R)
+do 
+    echo "Run size: $N"
+    python3 gen_data.py $N
+    circom dummy_main.circom --r1cs --wasm --sym --c --wat --output .
+    node dummy_main_js/generate_witness.js dummy_main_js/dummy_main.wasm input.json witness.wtns
+    npx snarkjs groth16 setup dummy_main.r1cs powers.ptau dummy_main_0.zkey
+    npx snarkjs zkey beacon dummy_main_0.zkey dummy_main.zkey 0102030405060708090a0b0c0d0e0f101112231415161718221a1b1c1d1e1f 10 -n="Final Beacon phase2"
+    npx snarkjs zkey verify dummy_main.r1cs powers.ptau dummy_main.zkey
+    npx snarkjs zkey export verificationkey dummy_main.zkey vkey.json
+    start=`date +%s%N`
+    npx snarkjs groth16 prove dummy_main.zkey witness.wtns proof.json public.json
+    end=`date +%s%N`
+    echo "Proving time: ($(echo "scale=6; $((end-start))/1000000" | bc)ms)"
+    npx snarkjs groth16 verify vkey.json public.json proof.json
+    N=$(expr "$N" "*" 2)
+done
