a47bae97edb26cff0249a0950671b2feb7f87333
Alluri Siddhartha <lalluri2@illinois.edu>
2023-11-26 15:12:55 -0600

    change msm fft

--- microbenches/msm/gnark/dummy_fft.go
+++ microbenches/msm/gnark/dummy_fft.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"runtime"
+	"strconv"
 	"time"
 
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fr/fft"
 )
+
+type FFTResult struct {
+	Framework string
+	Size      int
+	FFTtime   int64
+	IFFTtime  int64
+}
 
 func DummyFFT(n int) {
 	domain := fft.NewDomain(uint64(n))
@@ -20,9 +30,20 @@ func DummyFFT(n int) {
 
 	fft_time := time.Now()
 	domain.FFT(samples, fft.DIT)
+	fft_time_taken := time.Since(fft_time).Nanoseconds()
 	fmt.Println("FFT Time:", time.Since(fft_time))
 
 	ifft_time := time.Now()
 	domain.FFTInverse(samples, fft.DIF)
+	ifft_time_taken := time.Since(ifft_time).Nanoseconds()
 	fmt.Println("IFFT Time:", time.Since(ifft_time))
+	result := FFTResult{
+		Framework: "gnark",
+		Size:      n,
+		FFTtime:   fft_time_taken,
+		IFFTtime:  ifft_time_taken,
+	}
+
+	jsonfile, _ := json.MarshalIndent(result, "", " ")
+	_ = os.WriteFile(strconv.Itoa(n)+"fft_timing.json", jsonfile, 0644)
 }
--- microbenches/msm/gnark/run.sh
+++ microbenches/msm/gnark/run.sh
@@ -1,19 +1,24 @@
 go build -o gmt
-
+mkdir msm_data
+mv gmt msm_data/gmt
+cd msm_data
 ./gmt msm 1000
 ./gmt msm 10000
-./gmt msm 100000
-./gmt msm 1000000
-./gmt msm 10000000
+mv gmt ../gmt
+cd ..
 
+mkdir fft_data
+mv gmt fft_data/gmt
+cd fft_data
 ./gmt fft 1024
 ./gmt fft 8192
-./gmt fft 65536
-./gmt fft 524288
-./gmt fft 1048576
+mv gmt ../gmt
+cd ..
 
+mkdir circuit_data
+mv gmt circuit_data/gmt
+cd circuit_data
 ./gmt circuit 1000
 ./gmt circuit 10000
-./gmt circuit 100000
-./gmt circuit 1000000
-./gmt circuit 10000000
+mv gmt ../gmt
+cd ..
