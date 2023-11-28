a49016e82da84eccfcb8dee14af0351f695440f2
span14 <span14@illinois.edu>
2023-11-28 12:51:23 -0600

    update circom bench

--- microbenches/arithmetic/circom/bench.sh
+++ microbenches/arithmetic/circom/bench.sh
@@ -9,3 +9,5 @@ node ../../../circom/snarkjs/arithmetic.js 1048576
 node ../../../circom/snarkjs/arithmetic.js 2097152
 node ../../../circom/snarkjs/arithmetic.js 4194304
 node ../../../circom/snarkjs/arithmetic.js 8388608
+node ../../../circom/snarkjs/arithmetic.js 16777216
+node ../../../circom/snarkjs/arithmetic.js 33554432
