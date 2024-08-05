6c27c557863f36c2a490c4af49f2e50eab5a9586
Lilia Tang <liliatangxy@gmail.com>
2024-08-05 03:05:14 +0000

    fix golang version

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,8 +8,8 @@ sudo apt-get install build-essential cmake libgmp-dev libsodium-dev nasm curl m4
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
 
-wget https://go.dev/dl/go1.19.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.19.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.0.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
 curl -O https://bootstrap.pypa.io/get-pip.py
