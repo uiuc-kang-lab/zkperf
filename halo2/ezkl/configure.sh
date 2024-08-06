9b5e42514debf4677508790bd51cd6ebf404e86c
Lilia Tang <liliatangxy@gmail.com>
2024-08-06 03:48:19 +0000

    use recent rust for ezkl

--- halo2/ezkl/configure.sh
+++ halo2/ezkl/configure.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rustup toolchain install nightly-2024-07-31
+rustup override set nightly-2024-07-31
+
 cargo update -p "revm-interpreter" --precise 1.1.2
 cargo update -p "revm-precompile" --precise 2.0.3
 cargo update -p "revm-primitives" --precise 1.1.2
